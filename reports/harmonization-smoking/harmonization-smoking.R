905eadcccc10b39125dc2aec3ade1b883ffd8ea3
Andrey Koval <koval.andrey@gmail.com>
2016-03-28 13:20:13 -0700

    up

--- reports/harmonization-smoking/harmonization-smoking.R
+++ reports/harmonization-smoking/harmonization-smoking.R
@@ -50,9 +50,6 @@ dto[["metaData"]] %>% dplyr::select(study_name, name, item, construct, type, cat
 # ---- basic-table --------------------------------------------------------------
 
 # ---- basic-graph --------------------------------------------------------------
-# this is how we can interact with the `dto` to call and graph data and metadata
-dto[["metaData"]] %>% dplyr::filter(study_name=="share", name=="BR0030") %>% dplyr::select(name,label)
-dto[["unitData"]][["share"]] %>% dplyr::filter(!BR0030==9999) %>% histogram_continuous("BR0030", bin_width=5)
 
 
 # ----- view-metadata-1 ---------------------------------------------
@@ -62,24 +59,62 @@ meta_data <- dto[["metaData"]] %>%
   dplyr::arrange(construct, study_name)
 knitr::kable(meta_data)
 
-# ---- get-schema-candidates ---------------------------------------------
-# pull out the variables from the subsetted metadata
-# source("./scripts/common-functions.r")
-ds <- load_data_schema(dto=dto,
-                       varname_new="item", # column in metadata to provide new variable names
-                       construct_name = "smoking") # variables in this construct will be selected
+
+# ---- II-A-categorization-1 ----------------------
+dto[["metaData"]] %>% dplyr::filter(study_name=="share", name=="BR0030") %>% dplyr::select(name,label)
+dto[["unitData"]][["share"]] %>% dplyr::filter(!BR0030==9999) %>% histogram_continuous("BR0030", bin_width=1)
+
+dto[["metaData"]] %>% dplyr::filter(study_name=="tilda", name=="BH003") %>% dplyr::select(name,label)
+dto[["unitData"]][["tilda"]] %>% dplyr::filter(!BH003==-1) %>% histogram_continuous("BH003", bin_width=1)
 
 
-# ---- declare-schema-sets -------------------------
+# ---- II-A-categorization-2 ----------------------
+# categorize continuous variable BR0030 of SHARE
+ds <- dto[["unitData"]][["share"]]
+
+ds$BR0030_F[ds$BR0030 == 0]                      <- "less than 1"
+ds$BR0030_F[ds$BR0030 > 1  & ds$BR0030 <= 10   ] <- "1-10 years"
+ds$BR0030_F[ds$BR0030 > 11 & ds$BR0030 <= 20  ] <- "11-20 years"
+ds$BR0030_F[ds$BR0030 > 21 & ds$BR0030 <= 30  ] <- "21-30 years"
+ds$BR0030_F[ds$BR0030 > 31 & ds$BR0030 <= 40  ] <- "31-40 years"
+ds$BR0030_F[ds$BR0030 > 41 & ds$BR0030 <= 50  ] <- "41-50 years"
+ds$BR0030_F[ds$BR0030 > 51                    ] <- "51+ years"
+ds$BR0030_F[ds$BR0030 == 9999                  ] <- NA
+# convert to an ordered factor
+order_in_factor <- c("less than 1","1-10 years","11-20 years","21-30 years","31-40 years","41-50 years","51+ years")
+ds$BR0030_F <- ordered(ds$BR0030_F, labels = order_in_factor)
+ds %>% dplyr::group_by(BR0030_F) %>% dplyr::summarize(count=n())
+# attach modified dataset to dto, local to this report
+dto[["unitData"]][["share"]] <- ds
+
+# ---- II-A-categorization-3 ----------------------
+# categorize continuous variable BH003 of TILDA
+ds <- dto[["unitData"]][["tilda"]]
+ds$BH003_F <- car::Recode(ds$BH003, " -1 = NA; lo:25 ='YOUNG'; 26:50 = 'ADULT'; 51:75 = 'MIDDLEAGED'; 75:hi = 'OLD'")
+# convert to an ordered factor
+order_in_factor <- c("YOUNG","ADULT","MIDDLEAGED","OLD")
+ds$BH003_F <- ordered(ds$BH003_F, labels = order_in_factor)
+ds %>% dplyr::group_by(BH003_F) %>% dplyr::summarize(count=n())
+# attach modified dataset to dto, local to this report
+dto[["unitData"]][["tilda"]] <- ds
+
+# ---- II-A-2-schema-sets-1 -------------------------
 schema_sets <- list(
   "alsa" = c("SMOKER", "PIPCIGAR"),
   "lbsl" = c("SMK94","SMOKE"),
   "satsa" = c("GSMOKNOW", "GEVRSMK","GEVRSNS"),
-  "share" = c("BR0010","BR0020","BR0030cat" ), # "BR0030" is continuous
-  "tilda" = c("BH001","BH002", "BEHSMOKER","BH003cat") # "BH003" is continuous
+  "share" = c("BR0010","BR0020","BR0030_F" ), # "BR0030" is continuous
+  "tilda" = c("BH001","BH002", "BEHSMOKER","BH003_F") # "BH003" is continuous
 )
 
-# ---- export-response-profiles --------------------
+# ---- II-A-2-schema-sets-2 --------------------
+# view the joint profile of responses
+dto[["unitData"]][["alsa"]] %>% 
+  dplyr::group_by(SMOKER, PIPCIGAR) %>% 
+  dplyr::summarize(count = n()) 
+
+
+# ---- II-A-2-schema-sets-3 --------------------
 # define function to extract profiles
 response_profile <- function(dto, h_target, study, varnames_values){
   ds <- dto[["unitData"]][[study]]
@@ -90,26 +125,42 @@ response_profile <- function(dto, h_target, study, varnames_values){
   write.csv(d,paste0("./data/shared/derived/response-profiles/",h_target,"-",study,".csv"))
 }
 # extract response profile for data schema set from each study
-for(s in names(schema_sets))
+for(s in names(schema_sets)){
   response_profile(dto,
                    study = s,
                    h_target = 'smoking',
                    varnames_values = schema_sets[[s]]
-                   
-)
-
+                   )
+}
+ 
 # ---- target-1-alsa-1 -------------------------------------------------
-# view data schema candidates 
-dto[["metaData"]] %>% 
-  dplyr::filter(study_name=="alsa", construct == "smoking") %>% 
-  dplyr::select(name,label)
-
-# ---- target-1-alsa-2 -------------------------------------------------
 # view the joint profile of responses
-ds %>% 
-  dplyr::filter(study_name=="alsa") %>%
+dto[["unitData"]][["alsa"]] %>% 
   dplyr::group_by(SMOKER, PIPCIGAR) %>% 
   dplyr::summarize(count = n()) 
+
+
+# ---- target-1-alsa-2 -------------------------------------------------
+path_to_hrule <- "./data/shared/raw/response-profiles/h-rule-smoking-alsa.csv"
+(hrule <- read.csv(path_to_hrule, stringsAsFactors = F, na.strings = "NA"))
+
+study = "alsa"
+raw_vars = c("SMOKER", "PIPCIGAR")
+h_var = "smoke_now"
+recode_with_hrule <- function(dto, study, raw_vars, h_var){
+  d <- dto[["unitData"]][["alsa"]]
+  
+  d <- dto[["unitData"]][["alsa"]] %>% 
+  dplyr::group_by(SMOKER, PIPCIGAR) %>% 
+  dplyr::summarize(count = n()) %>%
+  dplyr::ungroup()
+  
+  
+  old_va
+  d[]
+  
+}
+
 
 # ---- target-1-alsa-3 -------------------------------------------------
 ds$SMOKER <- as.character(ds$SMOKER) 
