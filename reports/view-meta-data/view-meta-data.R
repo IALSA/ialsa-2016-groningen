2f04a6e9cd86db3303bbaf63eaca1bb83d5b587c
Andrey Koval <koval.andrey@gmail.com>
2016-04-01 08:45:14 -0700

    added dynamic table

---
+++ reports/view-meta-data/view-meta-data.R
@@ -0,0 +1,64 @@
+# knitr::stitch_rmd(script="./___/___.R", output="./___/___/___.md")
+#These first few lines run only when the file is run in RStudio, !!NOT when an Rmd/Rnw file calls it!!
+rm(list=ls(all=TRUE))  #Clear the variables from previous runs.
+cat("\f") # clear console 
+
+# ---- load-packages -----------------------------------------------------------
+# Attach these packages so their functions don't need to be qualified: http://r-pkgs.had.co.nz/namespace.html#search-path
+library(magrittr) # enables piping : %>% 
+
+# ---- load-sources ------------------------------------------------------------
+# Call `base::source()` on any repo file that defines functions needed below.  Ideally, no real operations are performed.
+source("./scripts/common-functions.R") # used in multiple reports
+source("./scripts/graph-presets.R") # fonts, colors, themes 
+
+# Verify these packages are available on the machine, but their functions need to be qualified: http://r-pkgs.had.co.nz/namespace.html#search-path
+requireNamespace("ggplot2") # graphing
+# requireNamespace("readr") # data input
+requireNamespace("tidyr") # data manipulation
+requireNamespace("dplyr") # Avoid attaching dplyr, b/c its function names conflict with a lot of packages (esp base, stats, and plyr).
+requireNamespace("testit")# For asserting conditions meet expected patterns.
+# requireNamespace("car") # For it's `recode()` function.
+
+# ---- declare-globals ---------------------------------------------------------
+
+# ---- load-data ---------------------------------------------------------------
+# load the product of 0-ellis-island.R,  a list object containing data and metadata
+dto <- readRDS("./data/unshared/derived/dto.rds")
+# each element this list is another list:
+names(dto)
+# 1st element - names of the studies as character vector
+dto[["studyName"]]
+(studyNames <- dto[["studyName"]])
+# 2nd element - file paths of the data files for each study
+dto[["filePath"]]
+# 3rd element - list objects with 
+names(dto[["dataFiles"]])
+dplyr::tbl_df(dto[["unitData"]][["alsa"]]) 
+# 4th element - dataset with augmented names and labels for variables from all involved studies
+dplyr::tbl_df(dto[["metaData"]])
+
+# ---- inspect-data -------------------------------------------------------------
+dto[["metaData"]] %>% dplyr::filter(study_name=="share", name=="BR0030") %>% dplyr::select(name,label)
+dto[["unitData"]][["share"]] %>% dplyr::filter(!BR0030==9999) %>% histogram_continuous("BR0030", bin_width=5)
+
+# ---- tweak-data --------------------------------------------------------------
+
+# ---- basic-table --------------------------------------------------------------
+
+# ---- basic-graph --------------------------------------------------------------
+# this is how we can interact with the `dto` to call and graph data and metadata
+dto[["metaData"]] %>% dplyr::filter(study_name=="share", name=="BR0030") %>% dplyr::select(name,label)
+dto[["unitData"]][["share"]] %>% dplyr::filter(!BR0030==9999) %>% histogram_continuous("BR0030", bin_width=5)
+
+# ---- dynamic-table -----------------------------------------------------------
+dto[["metaData"]]  %>%
+  dplyr::select(name, label, study_name,type,construct, item, categories) %>%
+  DT::datatable(class = 'cell-border stripe',
+              caption = "Use filtering and sorting to produce custom views to guide your inspection",
+               filter = "top", 
+              options = list(pageLength = 6, autoWidth = TRUE))
+
+# ---- reproduce ---------------------------------------
+rmarkdown::render(input = "./reports/view-meta-data/view-meta-data.Rmd" ,
+                  output_format="html_document", clean=TRUE)
