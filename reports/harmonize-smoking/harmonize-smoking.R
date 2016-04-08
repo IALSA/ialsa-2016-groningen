314eefc6e63e5cc55baabf66cd87af8b6aa5a8f6
Andrey Koval <koval.andrey@gmail.com>
2016-04-08 11:46:55 -0700

    #9 replaced categorization implementation
    before : recoding of continuous variables is accomplised
    programmatically in the script
    
    now: recode is done on empirical profile of responses using the external
    .csv

--- reports/harmonize-smoking/harmonize-smoking.R
+++ reports/harmonize-smoking/harmonize-smoking.R
@@ -84,13 +84,7 @@ categorization_profile <- function(dto, study, variable_name){
 }
 categorization_profile(dto,"share","BR0030")
 
-# import categories
-
-# apply categories
-
-
-
-
+# import and apply categorization rules
 study_name <- "share"
 path_to_crule <- "./data/meta/c-rules/c-rules-share-BR0030.csv"
 dto[["unitData"]][[study_name]] <- recode_with_crule(
@@ -99,46 +93,65 @@ dto[["unitData"]][[study_name]] <- recode_with_crule(
   variable_names = c("BR0030"), 
   categorization_name = "decades_smoked"
 )
-# verify
-dto[["unitData"]][["alsa"]] %>%
+# verify rules
+dto[["unitData"]][["share"]] %>%
   dplyr::filter(id %in% sample(unique(id),10)) %>%
-  dplyr::select_("id", "SMOKER", "PIPCIGAR", "smoke_now")
+  dplyr::select_("id", "BR0030", "decades_smoked")
+# frequencies
+dto[["unitData"]][["share"]] %>%
+  dplyr::group_by_("decades_smoked") %>% 
+  dplyr::summarise(average_years = mean(BR0030),
+                   sd = sd(BR0030),
+                   count = n())
 
-
-
-
-
-ds$BR0030_F[ds$BR0030 == 0]                      <- "less than 1"
-ds$BR0030_F[ds$BR0030 > 1  & ds$BR0030 <= 10   ] <- "1-10 years"
-ds$BR0030_F[ds$BR0030 > 11 & ds$BR0030 <= 20  ] <- "11-20 years"
-ds$BR0030_F[ds$BR0030 > 21 & ds$BR0030 <= 30  ] <- "21-30 years"
-ds$BR0030_F[ds$BR0030 > 31 & ds$BR0030 <= 40  ] <- "31-40 years"
-ds$BR0030_F[ds$BR0030 > 41 & ds$BR0030 <= 50  ] <- "41-50 years"
-ds$BR0030_F[ds$BR0030 > 51                    ] <- "51+ years"
-ds$BR0030_F[ds$BR0030 == 9999                  ] <- NA
 # convert to an ordered factor
-order_in_factor <- c("less than 1","1-10 years","11-20 years","21-30 years","31-40 years","41-50 years","51+ years")
-ds$BR0030_F <- ordered(ds$BR0030_F, labels = order_in_factor)
-ds %>% dplyr::group_by(BR0030_F) %>% dplyr::summarize(count=n())
+order_in_factor <- c("less than a year","1-10 years","11-20 years","21-30 years","31-40 years","41-50 years","51+ years")
+ds$decades_smoked <- ordered(ds$decades_smoked, labels = order_in_factor)
+ds %>% dplyr::group_by(decades_smoked) %>% dplyr::summarize(count=n())
 # attach modified dataset to dto, local to this report
 dto[["unitData"]][["share"]] <- ds
-dto[["unitData"]][["share"]] %>% histogram_discrete("BR0030_F")
+dto[["unitData"]][["share"]] %>% histogram_discrete("decades_smoked")
+
+
+
+
+
+
 # ---- II-A-categorization-3 ----------------------
+dto[["metaData"]] %>% dplyr::filter(study_name=="tilda", name=="BH003") %>% dplyr::select(name,label)
 dto[["unitData"]][["tilda"]] %>% dplyr::filter(!BH003==-1) %>% histogram_continuous("BH003", bin_width=1)
 # categorize continuous variable BH003 of TILDA
 ds <- dto[["unitData"]][["tilda"]]
-ds$BH003_F <- car::Recode(ds$BH003, "  -1 = NA; 
-                                    lo:25 ='YOUNG'; 
-                                    26:50 = 'ADULT'; 
-                                    51:75 = 'MIDDLEAGED'; 
-                                    75:hi = 'OLD'")
-# convert to an ordered factor
-order_in_factor <- c("YOUNG","ADULT","MIDDLEAGED","OLD")
-ds$BH003_F <- ordered(ds$BH003_F, labels = order_in_factor)
-ds %>% dplyr::group_by(BH003_F) %>% dplyr::summarize(count=n())
+
+
+# export categories
+categorization_profile(dto,"tilda","BH003")
+
+# import and apply categorization rules
+study_name <- "tilda"
+path_to_crule <- "./data/meta/c-rules/c-rules-tilda-BH003.csv"
+dto[["unitData"]][[study_name]] <- recode_with_crule(
+  dto,
+  study_name = study_name, 
+  variable_names = c("BH003"), 
+  categorization_name = "smoke_stop_decade"
+)
+# verify rules
+dto[["unitData"]][["tilda"]] %>%
+  dplyr::filter(id %in% sample(unique(id),10)) %>%
+  dplyr::select_("id", "BH003", "smoke_stop_decade")
+# frequencies
+dto[["unitData"]][["tilda"]] %>%
+  dplyr::group_by_("smoke_stop_decade") %>% 
+  dplyr::summarise(ave_duration_years = mean(BH003),
+                   sd = sd(BH003),
+                   count = n())
+
+dto[["unitData"]][["tilda"]] %>% dplyr::group_by(smoke_stop_decade) %>% dplyr::summarize(count=n())
 # attach modified dataset to dto, local to this report
-dto[["unitData"]][["tilda"]] <- ds
-dto[["unitData"]][["tilda"]] %>% histogram_discrete("BH003_F")
+dto[["unitData"]][["tilda"]] %>% histogram_discrete("smoke_stop_decade")
+
+
 
 # ---- II-A-2-schema-sets-1 -------------------------
 schema_sets <- list(
