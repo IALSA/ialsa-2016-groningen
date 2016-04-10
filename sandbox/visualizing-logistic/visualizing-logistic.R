aa60bc94b6652a0c562491c3a5ee0721911e592e
Andrey Koval <koval.andrey@gmail.com>
2016-04-10 09:04:42 -0700

    #18 update

--- sandbox/visualizing-logistic/visualizing-logistic.R
+++ sandbox/visualizing-logistic/visualizing-logistic.R
@@ -80,7 +80,7 @@ head(ds)
 # write(names(ds), "./data/unshared/derived/variable-names.txt", sep=" ")
 
 
-# ---- basic-graph -------------------------
+# ---- basic-info -------------------------
 
 # age summary across studies
 ds %>%  
@@ -109,26 +109,12 @@ table(ds$study_name, ds$educ3, useNA = "always")
 
 
 # ----- basic-model ------------------
-str(ds)
-d <- ds %>% na.omit()
-mdl <- glm(
-  formula = smoke_now ~ -1 + study_name + age_in_years +sex + marital + educ3,
-  # formula = smoke_now ~ -1 + study_name + age_in_years + sex ,
-  data = d
-  )
-summary(mdl)
-d$smoke_now_p <- predict(mdl)
-d <- d %>%  dplyr::select(id, study_name, age_in_years,sex, marital, educ3, smoke_now_p)
-# summary(mdl) # model summary
-# coefficients(mdl) # point estimates of model parameters (aka "model solution")
-# knitr::kable(vcov(mdl)) # covariance matrix of model parameters (inspect for colliniarity)
-# knitr::kable(cov2cor(vcov(mdl))) # view the correlation matrix of model parameters
-# confint(mdl, level=0.95) # confidence intervals for the estimated parameters
 
 
+# ---- define-elemental-graph-function -----------------------------
 
-graph_logistic_simple <- function(ds, x_name, y_name, color_group, alpha_level){
-  g <- ggplot2::ggplot(d, aes_string(x=x_name)) +
+graph_logistic_simple <- function(ds, x_name, y_name, color_group, alpha_level=.5){
+  g <- ggplot2::ggplot(ds, aes_string(x=x_name)) +
     geom_point(aes_string(y=y_name, color=color_group), shape=16, alpha=alpha_level) +
     facet_grid(study_name ~ .) + 
     main_theme +
@@ -137,9 +123,11 @@ graph_logistic_simple <- function(ds, x_name, y_name, color_group, alpha_level){
     )
   # return(g)
 }
-# graph_logistic_simple(ds,"age_in_years", "smoke_now_p", "sex", .3)
+# g <- graph_logistic_simple(ds=d,"age_in_years", "smoke_now_p", "sex", .3)
+# g
+# ---- define-complex-3-graph-function -----------------------
 
-graph_logistic_complex <- function(
+graph_logistic_complex_3 <- function(
   ds, 
   x_name, 
   y_name, 
@@ -162,8 +150,100 @@ graph_logistic_complex <- function(
   grid::popViewport(0)
   
 } 
+# graph_logistic_complex_3(ds=d,"age_in_years", "smoke_now_p", .3)
 
-graph_logistic_complex(ds,"age_in_years", "smoke_now_p", .3)
+
+# ---- define-complex-8-graph-function -----------------------
+graph_logistic_complex_8 <- function(
+  ds, 
+  x_name, 
+  y_name, 
+  alpha_level
+){
+  g_sex <- graph_logistic_simple(ds,x_name, y_name, "sex", alpha_level)
+  g_marital <- graph_logistic_simple(ds,x_name, y_name, "marital", alpha_level)
+  g_educ <- graph_logistic_simple(ds,x_name, y_name, "educ3", alpha_level)
+  
+  grid::grid.newpage()    
+  #Defnie the relative proportions among the panels in the mosaic.
+  layout <- grid::grid.layout(nrow=1, ncol=8,
+                              widths=grid::unit(c(.125, .125, .125, .125,
+                                                  .125, .125, .125, .125) ,
+                                                c("null","null","null","null",
+                                                  "null","null","null","null")),
+                              heights=grid::unit(c(1), c("null"))
+  )
+  grid::pushViewport(grid::viewport(layout=layout))
+  print(g_sex,     vp=grid::viewport(layout.pos.row=1, layout.pos.col=1 ))
+  print(g_marital, vp=grid::viewport(layout.pos.row=1, layout.pos.col=2 ))
+  print(g_educ,    vp=grid::viewport(layout.pos.row=1, layout.pos.col=3 ))
+  grid::popViewport(0)
+  
+} 
+# graph_logistic_complex_8(ds=d,"age_in_years", "smoke_now_p", .3)
+
+# ---- study-as-factor-in-model ----------------------------------------
+str(ds)
+d <- ds %>% na.omit()
+mdl <- glm(
+  formula = smoke_now ~ -1 + study_name + age_in_years +sex + marital + educ3,
+  # formula = smoke_now ~ -1 + study_name + age_in_years + sex ,
+  data = d
+);summary(mdl)
+d$smoke_now_p <- predict(mdl)
+d <- d %>%  dplyr::select(id, study_name, age_in_years, sex, marital, educ3, smoke_now_p)
+# summary(mdl) # model summary
+# coefficients(mdl) # point estimates of model parameters (aka "model solution")
+# knitr::kable(vcov(mdl)) # covariance matrix of model parameters (inspect for colliniarity)
+# knitr::kable(cov2cor(vcov(mdl))) # view the correlation matrix of model parameters
+# confint(mdl, level=0.95) # confidence intervals for the estimated parameters
+
+graph_logistic_complex_3(ds=d,"age_in_years", "smoke_now_p", .3)
+
+graph_logistic_complex_8(ds=d,"age_in_years", "smoke_now_p", .3)
+
+
+# ---- study-as-individual-data-set -------------------------
+
+model_outcome <- "smoke_now"
+model_predictors <- c("age_in_years", "sex", "marital", "educ3")
+
+ml <- list() # create a model list object to contain model estimation and modeled data
+for(s in dto[["studyName"]]){
+  d <- dto[['unitData']][[s]] %>% 
+    dplyr::select_(.dots=c("id",model_outcome, model_predictors)) %>% 
+    na.omit()
+  mdl <- stats::glm( # fit model
+    formula = smoke_now ~ age_in_years +sex + marital + educ3,
+    data = d, family="binomial"
+  ); summary(mdl); 
+  modeled_response_name <- paste0(model_outcome,"_p")
+  d[,modeled_response_name] <- predict(mdl)
+  ml[["data"]][[s]] <- d
+  ml[["model"]][[s]] <- mdl
+}
+names(ml[["data"]])
+names(ml[["model"]])
+
+d <- plyr::ldply(ml[["data"]],data.frame,.id = "study_name")
+d$id <- 1:nrow(d) # some ids values might be identical, replace
+head(d)
+
+g <- graph_logistic_simple(ds=d, "age_in_years","smoke_now_p","sex")
+g 
+graph_logistic_complex_3(ds=d, "age_in_years","smoke_now_p",.3)
+graph_logistic_complex_8(ds=d, "age_in_years","smoke_now_p",.3)
+
+
+# ----- dummy ---------------------------
+ld <- d %>%  dplyr::select(id, study_name, age_in_years,sex, marital, educ3, smoke_now_p)
+
+
+
+
+
+
+
 
 
 
@@ -189,7 +269,7 @@ str(summod)
 
 # ---- reproduce ---------------------------------------
 rmarkdown::render(
-  input = "./reports/harmonized-data/harmonized-data.Rmd" , 
+  input = "./sandbox/visualizing-logistic/visualizing-logistic.Rmd" , 
   output_format="html_document", clean=TRUE
 )
 
