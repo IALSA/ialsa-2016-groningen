3cecc2539dacf979362870f36d392e5821b263ca
Will Beasley <wibeasley@hotmail.com>
2016-04-21 12:27:19 -0500

    modifications for a different proposal

--- sandbox/visualizing-logistic/visualizing-logistic.R
+++ sandbox/visualizing-logistic/visualizing-logistic.R
@@ -7,8 +7,8 @@ cat("\f") # clear console
 # ---- load-sources ------------------------------------------------------------
 # Call `base::source()` on any repo file that defines functions needed below.  Ideally, no real operations are performed.
 source("./scripts/common-functions.R") # used in multiple reports
-source("./scripts/graph-presets.R") # fonts, colors, themes 
-source("./scripts/graph-logistic.R")
+# source("./scripts/graph-presets.R") # fonts, colors, themes 
+# source("./scripts/graph-logistic.R")
 
 # ---- load-packages -----------------------------------------------------------
 # Attach these packages so their functions don't need to be qualified: http://r-pkgs.had.co.nz/namespace.html#search-path
@@ -22,39 +22,14 @@ requireNamespace("plyr")
 requireNamespace("testit")# For asserting conditions meet expected patterns.
 
 # ---- declare-globals ---------------------------------------------------------
+possible_x <- seq.int(0, 150, 10)
+
 
 # ---- load-data ---------------------------------------------------------------
 # load the product of 0-ellis-island.R,  a list object containing data and metadata
 dto <- readRDS("./data/unshared/derived/dto.rds")
 
-# ---- inspect-data -------------------------------------------------------------
-# the list is composed of the following elements
-names(dto)
-# 1st element - names of the studies as character vector
-dto[["studyName"]]
-# 2nd element - file paths of the data files for each study as character vector
-dto[["filePath"]]
-# 3rd element - is a list object containing the following elements
-names(dto[["unitData"]])
-# each of these elements is a raw data set of a corresponding study, for example
-dplyr::tbl_df(dto[["unitData"]][["lbsl"]]) 
-
-# ---- meta-table --------------------------------------------------------
-# 4th element - a dataset names and labels of raw variables + added metadata for all studies
-# dto[["metaData"]] %>% 
-#   dplyr::select(study_name, name, item, construct, type, categories, label_short, label) %>%
-#   DT::datatable(
-#     class   = 'cell-border stripe',
-#     caption = "This is the primary metadata file. Edit at `./data/shared/meta-data-map.csv",
-#     filter  = "top",
-#     options = list(pageLength = 6, autoWidth = TRUE)
-#   )
-
 # ---- tweak-data --------------------------------------------------------------
-
-# ---- basic-table --------------------------------------------------------------
-
-# ---- basic-graph --------------------------------------------------------------
 
 # ---- assemble ------------------
 dmls <- list() # dummy list
@@ -78,51 +53,23 @@ for(s in dto[["studyName"]]){
 }
 lapply(dmls, names) # view the contents of the list object
 
-ds <- plyr::ldply(dmls,data.frame, .id = "study_name")
+ds <- dplyr::bind_rows(dmls, .id = "study_name")
 ds$id <- 1:nrow(ds) # some ids values might be identical, replace
 ds %>% dplyr::glimpse()
 
-# ---- save-for-Mplus ---------------------------
 
-# write.table(ds,"./data/unshared/derived/combined-harmonized-data-set.dat", row.names=F, col.names=F)
-# write(names(ds), "./data/unshared/derived/variable-names.txt", sep=" ")
-
-
-# ---- basic-info -------------------------
-# age summary across studies
-ds %>%  
-  dplyr::group_by(study_name) %>%
-  na.omit() %>% 
-  dplyr::summarize(
-    mean_age     = round(mean(age_in_years),1),
-    sd_age       = round(sd(age_in_years),2),
-    observed     = n(),
-    min_born     = min(year_born),
-    med_born     = median(year_born),
-    max_born     = max(year_born)
+# ---- create-fake-data-for-miechv-proposal ------------------------------------
+ds <- ds %>% 
+  dplyr::filter(
+    study_name != "share"
   ) %>% 
-  dplyr::ungroup()
+  dplyr::mutate(
+    age_in_years     = pmax(0, age_in_years-60)*150/40   ,
+    # study_name       = droplevels.factor(study_name, "share")
+    study_name       = factor(study_name, labels=c("Clinic 1", "Clinic 2", "Clinic 3", "Clinic 4"))
+  )
 
-# ---- age-frequencies ----------------                   
-# see counts across age groups and studies 
-t <- table(
-  cut(ds$age_in_years,breaks = c(-Inf,seq(from=40,to=100,by=5), Inf)),
-  ds$study_name, 
-  useNA = "always"
-); t[t==0] <- "."; t
-
-
-# ----- basic-frequencies-criteria-1 -------------------
-t <- table(ds$study_name, ds$smoke_now,     useNA = "always"); t[t==0] <- "."; t
-# ----- basic-frequencies-criteria-2 -------------------
-t <- table(ds$study_name, ds$smoked_ever,   useNA = "always"); t[t==0] <- "."; t
-# ----- basic-frequencies-predictors-1 -------------------
-t <- table(ds$study_name, ds$female,        useNA = "always"); t[t==0] <- "."; t
-# ----- basic-frequencies-predictors-2 -------------------
-t <- table(ds$study_name, ds$marital,       useNA = "always"); t[t==0] <- "."; t
-# ----- basic-frequencies-predictors-3 -------------------
-t <- table(ds$study_name, ds$educ3,         useNA = "always"); t[t==0] <- "."; t
-
+summary(ds$age_in_years)
 
 # ---- declare-variables  ----------------------------------------
 dv_name <- "smoke_now"
@@ -165,7 +112,7 @@ ds2$dv_p <- predict(model_global)
 
 ds_predicted_global <- expand.grid(
   study_name       = sort(unique(ds2$study_name)), #For the sake of repeating the same global line in all studies/panels in the facetted graphs
-  age_in_years     = seq.int(40, 100, 10),
+  age_in_years     = possible_x,
   female           = sort(unique(ds2$female)),
   educ3_f          = sort(unique(ds2$educ3_f)),
   marital_f        = sort(unique(ds2$marital_f)),
@@ -187,23 +134,19 @@ head(ds_predicted_global)
 # ---- compute-predicted-study ------------------------
 ds_predicted_study_list <- list()
 model_study_list <- list()
-for( study_name_ in dto[["studyName"]] ) {
+for( study_name_ in sort(unique(ds2$study_name)) ) {
   d_study <- ds2[ds2$study_name==study_name_, ]
   model_study <- glm(eq, data=d_study,  family=binomial(link="logit")) 
   model_study_list[[study_name_]] <- model_study
   
   d_predicted <- expand.grid(
-    age_in_years     = seq.int(40, 100, 10),
+    age_in_years     = possible_x,
     female           = sort(unique(ds2$female)),
     educ3_f          = sort(unique(ds2$educ3_f)),
     marital_f        = sort(unique(ds2$marital_f)),
     poor_health      = sort(unique(ds2$poor_health)),
     stringsAsFactors = FALSE
   ) 
-  
-  # d_predicted$dv_hat      <- as.numeric(predict(model_study, newdata=d_predicted)) #logged-odds of probability (ie, linear)
-  # d_predicted$dv_hat_p    <- plogis(d_predicted$dv_hat)                            #probability (ie, s-curve)
-  
   predicted_study           <- predict(model_study, newdata=d_predicted, se.fit=TRUE) 
   d_predicted$dv_hat        <- predicted_study$fit #logged-odds of probability (ie, linear)
   d_predicted$dv_upper      <- predicted_study$fit + 1.96*predicted_study$se.fit
@@ -243,7 +186,6 @@ ds_replicated_predicted_global_list <- list(
 )
 
 
-
 # ---- define-coloring-function ----------------
 assign_color <- function( d, facet_line ) {
   reference_color <- "#4daf4a" ##7e1a02 # 91777e
@@ -258,16 +200,12 @@ assign_color <- function( d, facet_line ) {
   descreased_risk_2 <- "#984ea3" # purple - further descrease in risk factor
   
   if( variable == "female") {
-    # http://colrd.com/image-dna/25114/
     palette_row <- c("TRUE"=reference_color, "FALSE"=increased_risk_1) # 98aab9
   } else if( variable %in% c("educ3", "educ3_f") ) { 
-    # http://colrd.com/image-dna/24382/
     palette_row <- c("high school"=reference_color, "less than high school"=increased_risk_1, "more than high school"=descreased_risk_1) # 54a992, e8c571
   } else if( variable %in% c("marital_f") ) {
-    # http://colrd.com/image-dna/23318/
     palette_row <- c("mar_cohab"=descreased_risk_1, "sep_divorced"= increased_risk_2, "single"=reference_color, "widowed"=increased_risk_1)
   } else if( variable %in% c("poor_health") ) {
-    # http://colrd.com/palette/18841/
     palette_row <- c("FALSE"=reference_color, "TRUE"=descreased_risk_2)
   } else {
     stop("The palette for this variable is not defined.")
@@ -342,14 +280,6 @@ ds_replicated_predicted_global <- ds_replicated_predicted_global_list %>% #This 
   )
 rm(ds_replicated_predicted_global_list)
 
-# ---- declare-reference-groups ------------------
-# reference_group <- c(
-#   "female"        = TRUE,
-#   "educ3_f"       = "high school",
-#   "marital_f"     = "single",
-#   "poor_health"   = FALSE
-# )
-
 # ---- replicated-predicted-local --------------------
 ds_replicated_predicted <- ds_replicated_predicted %>% 
   dplyr::arrange(prediction_line)
@@ -400,7 +330,13 @@ ds_replicated_predicted2 <- ds_replicated_predicted[ds_replicated_predicted$keep
 ds_replicated_predicted_global2 <- ds_replicated_predicted_global[ds_replicated_predicted_global$keep, ]
 
 # ---- model-plot ------------------
-graph_logistic_main <- function(ds){
+
+ds_replicated$facet_line <- ordered(ds_replicated$facet_line, labels=c("Gender", "Education", "Marriage", "Health"))
+ds_replicated_predicted2$facet_line <- ordered(ds_replicated_predicted2$facet_line, labels=c("Gender", "Education", "Marriage", "Health"))
+ds_replicated_predicted_global2$facet_line <- ordered(ds_replicated_predicted_global2$facet_line, labels=c("Gender", "Education", "Marriage", "Health"))
+
+graph_logistic_main <- function( ){
+  # [sample(x=nrow(ds_replicated), size=1000), ]
  ggplot(ds_replicated, aes(x=age_in_years, y=dv_hat_p, group=prediction_line, color=color_stroke)) +
     # geom_point(aes(y=as.integer(dv), group=NULL), shape=21, position=position_jitter(width=.3, height=.08), size=2, alpha=0.2, na.rm=T) +
     # geom_line(data=ds_replicated_predicted_global2, aes(group=NULL), color="gray60", size=4, alpha=.2, lineend="round") + #linetype="CC"
@@ -408,40 +344,13 @@ graph_logistic_main <- function(ds){
     # geom_ribbon(data=ds_replicated_predicted2, aes(ymax=dv_upper_p, ymin=dv_lower_p, group=NULL), color="gray80", alpha=.1) +
     geom_ribbon(data=ds_replicated_predicted_global2, aes(ymax=dv_upper_p, ymin=dv_lower_p, group=NULL), color="gray80", alpha=.1) +
     # geom_point(data=ds_replicated_predicted2) +
-    # scale_y_continuous(label=scales::percent, limits = c(0, .50)) +
     scale_y_continuous(label=scales::percent) +
     scale_color_identity() +
-    coord_cartesian(ylim = c(0, .5)) + 
+    coord_cartesian(xlim=c(0, 100), ylim = c(0, .25)) + 
     facet_grid(facet_line ~ study_name) +
     theme_light() +
     theme(legend.position="none") +
-    labs(x="Age", y=dv_label, title=eq_string)
+    labs(x="Duration (days)", y="P(Undesirable Outcome)", title="Risk by Clinic Location (columns) and by Subject Characteristics (rows)")
 }
-graph_logistic_main(ds_replicated)
-
-
-# ---- glm-support --------------------------
-# useful functions working with GLM model objects
-summary(mdl) # model summary
-coefficients(mdl) # point estimates of model parameters (aka "model solution")
-knitr::kable(vcov(mdl)) # covariance matrix of model parameters (inspect for colliniarity)
-knitr::kable(cov2cor(vcov(mdl))) # view the correlation matrix of model parameters
-confint(mdl, level=0.95) # confidence intervals for the estimated parameters
-
-# predict(mdl); fitted(mld) # generate prediction of the full model (all effects)
-# residuals(mdl) # difference b/w observed and modeled values
-anova(mdl) # put results into a familiary ANOVA table
-# influence(mdl) # regression diagnostics
-
-
-# create a model summary object to query 
-(summod <- summary(mdl))
-str(summod)
-
-
-
-# ---- reproduce ---------------------------------------
-rmarkdown::render(
-  input = "./sandbox/visualizing-logistic/visualizing-logistic.Rmd" , 
-  output_format="html_document", clean=TRUE
-)
+graph_logistic_main()
+ggsave("miechv-example.png", width=7, height=7, units="in", dpi=400)
