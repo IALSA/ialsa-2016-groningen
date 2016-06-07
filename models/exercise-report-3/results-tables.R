5e57a0286d63b45daa548b8ec083bc1a29c3b23d
Andrey Koval <koval.andrey@gmail.com>
2016-06-07 12:57:07 -0700

    tables of results

--- models/exercise-report-3/results-tables.R
+++ models/exercise-report-3/results-tables.R
@@ -7,7 +7,7 @@ cat("\f") # clear console
 # ---- load-sources ------------------------------------------------------------
 # Call `base::source()` on any repo file that defines functions needed below.  Ideally, no real operations are performed.
 source("./scripts/common-functions.R") # used in multiple reports
-
+source("./scripts/modeling-functions.R")
 # ---- load-packages -----------------------------------------------------------
 # Attach these packages so their functions don't need to be qualified: http://r-pkgs.had.co.nz/namespace.html#search-path
 library(magrittr) # enables piping : %>% 
@@ -50,6 +50,7 @@ ds_between %>%
     filter  = "top",
     options = list(pageLength = 6, autoWidth = TRUE)
   )
+
 # ----- dynamic-within ------------------------
 ds_within %>%
   DT::datatable(
@@ -59,5 +60,45 @@ ds_within %>%
     options = list(pageLength = 6, autoWidth = TRUE)
   )
 
+# ----- static-pooled -----------------
+cat("\n\n## pooled")
+cat("\n\n### BETWEEN")
+print(knitr::kable(tables_bw_pooled)) 
+
+for(model_type_ in c("A","B","AA","BB", "best") ){ 
+  table_to_print <- tables_pooled[[model_type_]]
+  cat("\n\n### ", model_type_)
+  cat("\n\n ", "solution of model **", model_type_, "** fit to combined and harmonized data from ** ALL ** studies", sep="")
+  print(knitr::kable(basic_model_info(models_pooled[[model_type_]])))
+  print(knitr::kable(table_to_print))
+} 
+
+# # ----- static-between -----------------
+# 
+# 
+# for(study_name_ in dto$studyName ){
+#  table_to_print <- tables_bw_local[[study_name_]]
+#  cat("\n\n### ", study_name_)
+#  cat("\n\n Comparing models fit to ", study_name_,"'s data")
+#  print(knitr::kable(table_to_print))
+# }
+# print(knitr::kable(tables_bw_pooled))
+
+# ----- static-local -----------------------
+# names(tables_local)
+# study_name_ = "alsa"
+# model_type_ = "A"
+for(study_name_ in dto$studyName ){
+  cat("\n\n## ", study_name_)
+  table_to_print <- tables_bw_local[[study_name_]]
+  cat("\n\n### BETWEEN")
+  for(model_type_ in c("A","B","AA","BB", "best") ){
+  table_to_print <- tables_local[[model_type_]][[study_name_]]
+  cat("\n\n### ", model_type_)
+  cat("\n\n ", "solution of model **", model_type_, "** fit to data from **", study_name_,"** study", sep="")
+  print(knitr::kable(basic_model_info(models_local[[model_type_]][[study_name_]])))
+  print(knitr::kable(table_to_print))
+  }
+}
 
 
