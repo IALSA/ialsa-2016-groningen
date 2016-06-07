f29125b6ef706e8b03a23c02f196a0a74cbb3d7f
Andrey Koval <koval.andrey@gmail.com>
2016-06-07 10:36:03 -0700

    reports with nothing but tables
    Prototypical appendix

---
+++ models/exercise-report-3/results-tables.R
@@ -0,0 +1,63 @@
+# This report conducts harmonization procedure 
+# knitr::stitch_rmd(script="./___/___.R", output="./___/___/___.md")
+#These first few lines run only when the file is run in RStudio, !!NOT when an Rmd/Rnw file calls it!!
+rm(list=ls(all=TRUE))  #Clear the variables from previous runs.
+cat("\f") # clear console
+
+# ---- load-sources ------------------------------------------------------------
+# Call `base::source()` on any repo file that defines functions needed below.  Ideally, no real operations are performed.
+source("./scripts/common-functions.R") # used in multiple reports
+
+# ---- load-packages -----------------------------------------------------------
+# Attach these packages so their functions don't need to be qualified: http://r-pkgs.had.co.nz/namespace.html#search-path
+library(magrittr) # enables piping : %>% 
+library(ggplot2)
+
+# Verify these packages are available on the machine, but their functions need to be qualified: http://r-pkgs.had.co.nz/namespace.html#search-path
+requireNamespace("ggplot2") # graphing
+requireNamespace("tidyr") # data manipulation
+requireNamespace("dplyr") # Avoid attaching dplyr, b/c its function names conflict with a lot of packages (esp base, stats, and plyr).
+requireNamespace("plyr")
+requireNamespace("testit")# For asserting conditions meet expected patterns.
+
+# ---- declare-globals ---------------------------------------------------------
+
+# ---- load-data ---------------------------------------------------------------
+# prepared by Ellis Island and ./reports/report-governor.R
+dto <- readRDS("./data/unshared/derived/dto_h.rds")
+
+# prepared by ../compile-models.R
+models_pooled <- readRDS("./data/shared/derived/models/models_pooled.rds")#  glm objects
+subset_pooled <- readRDS("./data/shared/derived/models/subset_pooled.rds")#  glmulti objects
+
+models_local <- readRDS("./data/shared/derived/models/models_local.rds")
+subset_local <- readRDS("./data/shared/derived/models/subset_local.rds")
+
+# prepared by ../compile-tables.R
+tables_pooled <- readRDS("./data/shared/derived/tables/tables_pooled.rds")
+tables_local <- readRDS("./data/shared/derived/tables/tables_local.rds")
+tables_bw_pooled <- readRDS("./data/shared/derived/tables/tables_bw_pooled.rds")
+tables_bw_local <- readRDS("./data/shared/derived/tables/tables_bw_local.rds")
+
+ds_within <- readRDS("./data/shared/derived/tables/ds_within.rds")
+ds_between <- readRDS("./data/shared/derived/tables/ds_between.rds")
+
+# ----- dynamic-between -----------------------
+ds_between %>%
+  DT::datatable(
+    class   = 'cell-border stripe',
+    caption = "Comparison across models || identifiable by : study_name",
+    filter  = "top",
+    options = list(pageLength = 6, autoWidth = TRUE)
+  )
+# ----- dynamic-within ------------------------
+ds_within %>%
+  DT::datatable(
+    class   = 'cell-border stripe',
+    caption = "Individual model solution || identifiable by : study_name and model_type",
+    filter  = "top",
+    options = list(pageLength = 6, autoWidth = TRUE)
+  )
+
+
+
